b9cfc8975e27ee08150606dbf0926a7771618259
Abhilash Jha <abhilashjha2004@gmail.com>
2025-12-19 01:25:35 +0530

    updated for deploy

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = "/api/predict";
 let chartInstances = { performance: null, feature: null };
 let predictionHistory = [];
 let evaluationResults = null;
